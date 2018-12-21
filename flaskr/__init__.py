d6aff063cb42545b2b0147c45b75d57c5ba8e685
bbcool4444 <bbcool4444@163.com>
2018-12-21 17:35:23 +0800

    build setup

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,8 +2,9 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
-    app = Flask(__name__, intance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -19,8 +20,14 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return('Hello, World!')
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return(app)
